f69832c1995d3649d579339097dccfe36c642e59
Nam Joon <jungeol66104@gmail.com>
2024-11-18 10:42:15 +0900

    sticky 2 fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,9 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
   return (
     <html>
       <body>
-        <div className={'w-full h-full flex flex-col items-center justify-center'}>
-            <div className={'w-full max-w-[640px] h-full'}>
-                <div className={'z-10 sticky top-0 left-0 w-full h-[50px] flex items-center justify-center bg-black'}><Link href={'/'} className={`shrink-0 font-[700] text-2xl`} style={{fontFamily: "'ubuntu', Arial, sans-serif"}}>shortt</Link></div>
-                <div className={'relative w-full h-[calc(100%-50px)] flex flex-col items-center'}>{children}</div>
-            </div>
+        <div className={'z-10 fixed top-0 left-0 w-full h-[50px] flex items-center justify-center bg-black'}><Link href={'/'} className={`shrink-0 font-[700] text-2xl`} style={{fontFamily: "'ubuntu', Arial, sans-serif"}}>shortt</Link></div>
+        <div className={'mt-[50px] w-full h-[calc(100%-50px)] flex justify-center'}>
+            <div className={'relative w-full max-w-[640px] h-full flex flex-col items-center'}>{children}</div>
         </div>
       </body>
     </html>
